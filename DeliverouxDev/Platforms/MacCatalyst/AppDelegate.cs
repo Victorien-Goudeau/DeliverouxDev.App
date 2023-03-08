dd56115a4615fe94715dc600a8bc0eef3353ba79
Victorien Goudeau <vgoudeau@sqli.com>
2023-03-08 13:03:07 +0100

    Ajoutez des fichiers projet.

---
+++ DeliverouxDev/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DeliverouxDev;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
